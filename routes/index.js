f5c0ed5bac35c2086ba4b62baa0afd040842bdfa
kunuutkaali <kunuutkaali@gmail.com>
2023-01-23 12:28:40 +0100

    Opgave 1, done

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About page' });
+});
+
 
 module.exports = router;
